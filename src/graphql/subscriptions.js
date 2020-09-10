0eef06971ead853be67d2dbd662a107aa25819b9
Aaron Thompson <thompson_ad@outlook.com>
2020-09-10 21:13:33 +0100

    add gql api

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateNote = /* GraphQL */ `
+  subscription OnCreateNote {
+    onCreateNote {
+      id
+      clientId
+      description
+      completed
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateNote = /* GraphQL */ `
+  subscription OnUpdateNote {
+    onUpdateNote {
+      id
+      clientId
+      description
+      completed
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteNote = /* GraphQL */ `
+  subscription OnDeleteNote {
+    onDeleteNote {
+      id
+      clientId
+      description
+      completed
+      createdAt
+      updatedAt
+    }
+  }
+`;
